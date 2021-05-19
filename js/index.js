e94bb8efbfff8739ca1bd89e41aae03ea566ff5a
Dmitriy <dmitriypruss21041984@gmail.com>
2021-05-19 23:15:18 +0300

    9 commit

--- js/index.js
+++ js/index.js
@@ -178,9 +178,8 @@ function showFunctions() {
             const [a, b, c] = getTriangleSides();
             if (a + b > c && a + c > b && b + c > a) {
                 return true;
-            } else {
-                return false;
             };
+            return false;
         };
     console.groupEnd();
 
